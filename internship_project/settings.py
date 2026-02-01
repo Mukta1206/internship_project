41b2ef80434ec411704ea23b41152c4a66ce3eeb
Mukta Tanksale <muktatanksale1206@gmail.com>
2026-01-31 23:00:42 -0500

    Disable DEBUG and lock ALLOWED_HOSTS for production

--- internship_project/settings.py
+++ internship_project/settings.py
@@ -35,10 +35,10 @@ SECRET_KEY = 'django-insecure-5+jvyk8qppeumzsqn1$p8-7sm6p2+b0y4#tn*!+xjw@tf2x20%
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["internship-project-oan9.onrender.com"]
 
 
 # Application definition
